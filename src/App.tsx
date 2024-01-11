68520d415b9a7de5ecb038560a7f79ac648a8a68
hiro266 <t1freejob@gmail.com>
2024-01-12 00:50:03 +0900

    疎通確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState} from 'react'
+import axios from 'axios';
 import './App.css';
 
+type User = {
+  id: string;
+  name: string;
+}
+
 function App() {
+
+  const [users, setUsers] = useState<User[]>([])
+
+  const onClickProblem = () => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res => {
+      setUsers(res.data)
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={onClickProblem}>問題</button>
+      {users.map((user, index) => (
+       <li key={index}>{user.name}</li> 
+      ))} 
     </div>
   );
 }
